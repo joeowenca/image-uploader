66eb32c701dd0ec51898123df1236218c0e90e53
Joe Owen <joe.a.owen@gmail.com>
2023-03-09 14:38:34 -0800

    Use async await to better handle API calls

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import readDirectory from './readDirectory.js'
-import uploadImages from './uploadImages.js'
-
-const images = { name: "Images", children: [] };
- 
-uploadImages(readDirectory(process.argv[2]), images);
-
-fs.writeFileSync('manifest.json', JSON.stringify(images, null, 2));
